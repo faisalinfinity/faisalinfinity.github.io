1387c36cf5a08a01f532774a80a10c0e7488e3f5
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-05-05 22:11:55 +0530

    added new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,6 +28,8 @@ import {
   SiNodedotjs,
   SiReact,
   SiRedux,
+  SiSocketdotio,
+  SiTypescript,
 } from "react-icons/si";
 import { ScrollContext } from "../Context/ScrollContext";
 import "../styles/project.css";
@@ -52,6 +54,26 @@ const testimonials = [
     netlify: "https://frontend-snowy-eta.vercel.app/",
     light:
       "https://res.cloudinary.com/dq674z2lp/image/upload/v1680457612/mobile_xgjbgy.png",
+  },
+  {
+    role: "Captain America - A Multiplayer Game with Realtime Global chat and scores built within 48hrs.",
+    content: "Captain America - Game  (Full Stack App)",
+    l1: "Mongodb",
+    l2: "Typescript",
+    l3: "React",
+    l4: "NodeJS",
+    l5: "Socket.io",
+    l6: "Bcrypt",
+    Icon1: SiMongodb,
+    Icon3: SiNodedotjs,
+    Icon4: SiReact,
+    Icon2: SiTypescript,
+    Icon5: SiSocketdotio,
+    url: "https://res.cloudinary.com/dq674z2lp/image/upload/v1681144295/mobile_3_ty3cgl.png",
+    github: "https://github.com/faisalinfinity/Captain-America-The-Game",
+    netlify: "https://captainamerica.vercel.app",
+    light:
+      "https://res.cloudinary.com/dq674z2lp/image/upload/v1681144285/mobile_5_usvxm5.png",
   },
   {
     role: "Productify is an Ecommerce Website which serves as a platform by offering buyers and sellers a wide range of choice to buy and sell goods through listing format, item condition and delivery format.",
@@ -151,7 +173,6 @@ const testimonials = [
     netlify: "https://incomparable-sable-d0bb36.netlify.app/",
     light: "https://i.postimg.cc/DfQyvMWM/tc-dark.png",
   },
-  
 ];
 
 export const backgrounds = [
@@ -160,7 +181,6 @@ export const backgrounds = [
   `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='102.633' cy='61.0737' rx='102.633' ry='61.0737' fill='%23ED8936'/%3E%3Cellipse cx='399.573' cy='123.926' rx='102.633' ry='61.0737' fill='%2348BB78'/%3E%3Cellipse cx='366.192' cy='73.2292' rx='193.808' ry='73.2292' fill='%230BC5EA'/%3E%3Cellipse cx='222.705' cy='110.585' rx='193.808' ry='73.2292' fill='%23ED64A6'/%3E%3C/svg%3E")`,
   `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='457.367' cy='123.926' rx='102.633' ry='61.0737' transform='rotate(-180 457.367 123.926)' fill='%23ECC94B'/%3E%3Cellipse cx='160.427' cy='61.0737' rx='102.633' ry='61.0737' transform='rotate(-180 160.427 61.0737)' fill='%239F7AEA'/%3E%3Cellipse cx='193.808' cy='111.771' rx='193.808' ry='73.2292' transform='rotate(-180 193.808 111.771)' fill='%234299E1'/%3E%3Cellipse cx='337.295' cy='74.415' rx='193.808' ry='73.2292' transform='rotate(-180 337.295 74.415)' fill='%2348BB78'/%3E%3C/svg%3E")`,
 ];
-
 
 function TestimonialCard(props) {
   const {
@@ -351,7 +371,7 @@ function TestimonialCard(props) {
 }
 
 export default function Projects({ projectRef, device }) {
-  const { scrollref} = useContext(ScrollContext);
+  const { scrollref } = useContext(ScrollContext);
 
   useEffect(() => {
     scrollref?.current.scrollIntoView({ behavior: "smooth" });
